bd0789b752caa29178216451738f96f3abd08a56
PiotrRogulski <piter.rogulski@gmail.com>
2022-12-01 17:19:41 +0100

    [D1] Cleanup

--- src/Days/Day1.hs
+++ src/Days/Day1.hs
@@ -3,9 +3,9 @@
 module Days.Day1 (module Days.Day1) where
 
 import Data.Functor ((<&>))
+import Data.List (sortBy)
 import Data.Text (Text, pack, splitOn, unpack)
 import DayInput (getDay)
-import Data.List (sortBy)
 
 sums :: IO [Int]
 sums = getDay 1 <&> splitOn ("\n\n" :: Text) . pack <&> map unpack <&> map lines <&> map (map (read @Int)) <&> map sum
