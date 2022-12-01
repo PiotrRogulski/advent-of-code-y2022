d9f31d0581b39988e37aca7626203577d06a076d
PiotrRogulski <piotr.rogulski@leancode.pl>
2022-12-01 10:02:22 +0100

    [D1] Part 1

--- src/Days/Day1.hs
+++ src/Days/Day1.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Days.Day1 (module Days.Day1) where
 
-pt1 :: IO ()
-pt1 = do
-  putStrLn "part 1"
+import Data.Functor ((<&>))
+import Data.Text (Text, pack, splitOn, unpack)
+import DayInput (getDay)
+
+pt1 :: IO Int
+pt1 = getDay 1 <&> splitOn ("\n\n" :: Text) . pack <&> map unpack <&> map lines <&> map (map (read @Int)) <&> map sum <&> foldl max 0
 
 pt2 :: IO ()
 pt2 = do
