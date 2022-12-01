1ee4ddc4dd0118d3b70a504b31079a7d5e4f509e
PiotrRogulski <piotr.rogulski@leancode.pl>
2022-12-01 10:06:20 +0100

    [D1] Part 2

--- src/Days/Day1.hs
+++ src/Days/Day1.hs
@@ -5,10 +5,13 @@ module Days.Day1 (module Days.Day1) where
 import Data.Functor ((<&>))
 import Data.Text (Text, pack, splitOn, unpack)
 import DayInput (getDay)
+import Data.List (sortBy)
+
+sums :: IO [Int]
+sums = getDay 1 <&> splitOn ("\n\n" :: Text) . pack <&> map unpack <&> map lines <&> map (map (read @Int)) <&> map sum
 
 pt1 :: IO Int
-pt1 = getDay 1 <&> splitOn ("\n\n" :: Text) . pack <&> map unpack <&> map lines <&> map (map (read @Int)) <&> map sum <&> foldl max 0
+pt1 = sums <&> foldl max 0
 
-pt2 :: IO ()
-pt2 = do
-  putStrLn "part 2"
+pt2 :: IO Int
+pt2 = sums <&> sortBy (flip compare) <&> take 3 <&> sum
