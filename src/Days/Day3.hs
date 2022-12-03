85fb45cb71c8ce5d0e8057c1d6956d70f84f947e
PiotrRogulski <piter.rogulski@gmail.com>
2022-12-03 08:43:26 +0100

    [D3] Part 2

--- src/Days/Day3.hs
+++ src/Days/Day3.hs
@@ -3,6 +3,7 @@ module Days.Day3 (module Days.Day3) where
 import Data.Char (ord)
 import Data.Functor ((<&>))
 import Data.List (intersect, nub)
+import Data.List.Split (chunksOf)
 import DayInput (getDay)
 
 itemValue :: Char -> Int
@@ -16,12 +17,28 @@ splitInHalf xs = splitAt half xs
   where
     half = length xs `div` 2
 
+intersect3 :: Eq a => [[a]] -> [a]
+intersect3 [a, b, c] = a `intersect` b `intersect` c
+intersect3 _ = error "Invalid input"
+
+------------------------------------------------------------
+
 pt1 :: IO Int
 pt1 =
   getDay 3
     <&> lines
     <&> map splitInHalf
     <&> map (uncurry intersect)
+    <&> map nub
+    <&> map (sum . map itemValue)
+    <&> sum
+
+pt2 :: IO Int
+pt2 =
+  getDay 3
+    <&> lines
+    <&> chunksOf 3
+    <&> map intersect3
     <&> map nub
     <&> map (sum . map itemValue)
     <&> sum
