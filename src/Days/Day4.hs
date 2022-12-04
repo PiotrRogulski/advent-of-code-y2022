9bcff0767324d7ba516e233e1c06a1dec6c1e19c
PiotrRogulski <piter.rogulski@gmail.com>
2022-12-04 08:57:07 +0100

    [D2] Part 2

--- src/Days/Day4.hs
+++ src/Days/Day4.hs
@@ -13,8 +13,11 @@ data Range = Range Int Int deriving (Show)
 isSubsetOf :: Range -> Range -> Bool
 (Range a b) `isSubsetOf` (Range c d) = a >= c && b <= d
 
-hasOverlap :: Range -> Range -> Bool
-r1 `hasOverlap` r2 = r1 `isSubsetOf` r2 || r2 `isSubsetOf` r1
+hasCompleteOverlap :: Range -> Range -> Bool
+r1 `hasCompleteOverlap` r2 = r1 `isSubsetOf` r2 || r2 `isSubsetOf` r1
+
+hasPartialOverlap :: Range -> Range -> Bool
+(Range a b) `hasPartialOverlap` (Range c d) = a <= d && b >= c
 
 parseLine :: String -> (Range, Range)
 parseLine s = (Range (read a) (read b), Range (read c) (read d))
@@ -26,5 +29,13 @@ pt1 =
   getDay 4
     <&> lines
     <&> map parseLine
-    <&> filter (uncurry hasOverlap)
+    <&> filter (uncurry hasCompleteOverlap)
+    <&> length
+
+pt2 :: IO Int
+pt2 =
+  getDay 4
+    <&> lines
+    <&> map parseLine
+    <&> filter (uncurry hasPartialOverlap)
     <&> length
