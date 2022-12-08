9d419b01417cd5586cc89506ecec3717c03e79b9
PiotrRogulski <piter.rogulski@gmail.com>
2022-12-08 21:42:22 +0100

    [D8] Part 2

--- src/Days/Day8.hs
+++ src/Days/Day8.hs
@@ -1,9 +1,11 @@
 module Days.Day8 (module Days.Day8) where
 
-import Control.Arrow (Arrow (second))
+import Control.Arrow (Arrow (second, (***)))
 import Data.Char (ord)
+import Data.Function ((&))
 import Data.Functor ((<&>))
 import Data.List (transpose)
+import Data.List.HT (takeUntil)
 import DayInput (getDay)
 
 isTreeVisible :: [[Int]] -> Int -> Int -> Bool
@@ -13,6 +15,14 @@ isTreeVisible trees' x y = any (all (< tree)) lists
     lists = [left, right, top, bottom]
     (left, right) = second (drop 1) $ splitAt y (trees' !! x)
     (top, bottom) = second (drop 1) $ splitAt x (transpose trees' !! y)
+
+scenicScore :: [[Int]] -> Int -> Int -> Int
+scenicScore trees' x y = lists & map length & product
+  where
+    tree = trees' !! x !! y
+    lists = map (takeUntil (>= tree)) [left, right, top, bottom]
+    (left, right) = (reverse *** drop 1) $ splitAt y (trees' !! x)
+    (top, bottom) = (reverse *** drop 1) $ splitAt x (transpose trees' !! y)
 
 trees :: IO [[Int]]
 trees =
@@ -32,5 +42,19 @@ processTrees1 trees' =
     width = length $ head trees'
     height = length trees'
 
+processTrees2 :: [[Int]] -> Int
+processTrees2 trees' =
+  [ scenicScore trees' x y
+    | x <- [0 .. height - 1],
+      y <- [0 .. width - 1]
+  ]
+    & maximum
+  where
+    width = length $ head trees'
+    height = length trees'
+
 pt1 :: IO Int
 pt1 = trees <&> processTrees1
+
+pt2 :: IO Int
+pt2 = trees <&> processTrees2
