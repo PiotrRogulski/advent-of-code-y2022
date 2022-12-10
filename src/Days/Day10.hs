4e869b3d46bf8d94a6e7d1db402a98b9f82eff0f
PiotrRogulski <piter.rogulski@gmail.com>
2022-12-10 11:04:31 +0100

    [D10] Part 2

--- src/Days/Day10.hs
+++ src/Days/Day10.hs
@@ -1,8 +1,10 @@
 module Days.Day10 (module Days.Day10) where
 
+import Control.Arrow ((>>>))
 import Data.Function ((&))
 import Data.Functor ((<&>))
 import Data.List (scanl')
+import Data.List.Split (chunksOf)
 import DayInput (getDay)
 
 data Command = Noop | AddX Int deriving (Show, Eq)
@@ -36,3 +38,17 @@ pt1 =
   input
     <&> pickSignals [20, 60 .. 220]
     <&> sum
+
+screenWidth :: Int
+screenWidth = 40
+
+charForSignalInTime :: Int -> Int -> Char
+charForSignalInTime t n = if abs (n - (t `mod` screenWidth)) <= 1 then '#' else '.'
+
+pt2 :: IO ()
+pt2 =
+  input
+    >>= ( zipWith charForSignalInTime [0 ..]
+            >>> chunksOf screenWidth
+            >>> mapM_ putStrLn
+        )
