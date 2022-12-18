94ad6eb24d26b8e072619c2e97783e205f26bcd3
PiotrRogulski <piter.rogulski@gmail.com>
2022-12-18 13:53:32 +0100

    [D18] Part 2

--- src/Days/Day18.hs
+++ src/Days/Day18.hs
@@ -1,7 +1,11 @@
 module Days.Day18 (module Days.Day18) where
 
-import Control.Arrow (Arrow (second), (&&&), (***))
+import Control.Arrow (Arrow (second), (&&&), (***), (>>>))
+import Control.Lens ((??))
+import Data.Foldable (foldr')
+import Data.Function ((&))
 import Data.Functor ((<&>))
+import Data.Set qualified as S
 import DayInput (getDay)
 
 type Coordinate = (Int, Int, Int)
@@ -38,3 +42,64 @@ pt1 =
     <&> ((* 6) *** length)
     <&> second (* 2)
     <&> uncurry (-)
+
+getBounds :: [Coordinate] -> (Coordinate, Coordinate)
+getBounds points =
+  let (x, y, z) = unzip3 points
+   in ((minimum x - 1, minimum y - 1, minimum z - 1), (maximum x + 1, maximum y + 1, maximum z + 1))
+
+isInBounds :: (Coordinate, Coordinate) -> Coordinate -> Bool
+isInBounds ((x1, y1, z1), (x2, y2, z2)) (x, y, z) =
+  x >= x1
+    && x <= x2
+    && y >= y1
+    && y <= y2
+    && z >= z1
+    && z <= z2
+
+neighbors :: Coordinate -> [Coordinate]
+neighbors (x, y, z) =
+  [ (x + 1, y, z),
+    (x - 1, y, z),
+    (x, y - 1, z),
+    (x, y + 1, z),
+    (x, y, z - 1),
+    (x, y, z + 1)
+  ]
+
+walk :: S.Set Coordinate -> S.Set Coordinate -> Int -> (Coordinate, Coordinate) -> S.Set Coordinate -> Int
+walk queue done acc bounds points
+  | null queue = acc
+  | otherwise =
+      walk
+        (foldr' S.insert queue' unexploredAir)
+        (S.insert currentPoint done)
+        (acc + length neighbouringLava)
+        bounds
+        points
+  where
+    (currentPoint, queue') = S.deleteFindMin queue
+    unexploredAir =
+      currentPoint
+        & neighbors
+        & filter
+          ( ( [ not . (`S.member` points),
+                not . (`S.member` done),
+                isInBounds bounds
+              ]
+                ??
+            )
+              >>> and
+          )
+    neighbouringLava =
+      currentPoint
+        & neighbors
+        & filter (`S.member` points)
+
+theSearch :: [Coordinate] -> Int
+theSearch points =
+  let bounds = getBounds points
+   in walk (S.singleton (fst bounds)) S.empty 0 bounds (S.fromList points)
+
+pt2 :: IO Int
+pt2 = input <&> theSearch
